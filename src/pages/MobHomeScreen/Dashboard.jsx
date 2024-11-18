1e7579bdb8c2016da5d79942c954be49d2adca6e
Anirudh Bhardwaj <anirudhbhardwaj@Anirudhs-MacBook-Air.local>
2024-11-18 15:23:06 +0530

    card control options

--- src/pages/MobHomeScreen/Dashboard.jsx
+++ src/pages/MobHomeScreen/Dashboard.jsx
@@ -2,6 +2,36 @@ import React from 'react'
 import './MobHomeScreen.css'
 import useFetchUserData from '../../hooks/Auth/useFetchUserData'
 import { Link } from 'react-router-dom'
+import payments from '../../assets/svg/payment.avif'
+import Teams from '../../assets/svg/teams.jpg'
+import shortlisted from '../../assets/svg/shorlisted.webp'
+import xperts from '../../assets/svg/xperts.avif'
+
+
+
+const orgMenu = [
+  {
+    title: 'Shortlisted',
+    path: 'teams/shortlisted',
+    icon: shortlisted
+  },
+  {
+    title: 'Xperts',
+    path: 'teams/xtern',
+    icon: xperts
+  },
+  {
+    title: 'Teams',
+    path: 'teams/colleagues',
+    icon: Teams
+  },
+  {
+    title: 'Payments',
+    path: 'teams/payments',
+    icon: payments
+  }
+]
+
 
 export default function Dashboard() {
 
@@ -20,11 +50,17 @@ export default function Dashboard() {
 
          </div>
          
-         <div className='mob-control-btn-container'>
-               <Link className='mob-control-btns'>Shortlisted</Link>
-               <Link className='mob-control-btns'>Xtern</Link>
-               <Link className='mob-control-btns'>Collegues</Link>
-               <Link className='mob-control-btns'>Payments</Link>
+         <div className='mob-control-btn-container'>   
+                {
+                  orgMenu.map((options)=>(
+                    <Link className='mob-control-btns' to={options.path}>
+                    <span className='control-menu-title'>{options.title}</span>
+                    <img 
+                    className='control-menu-icon'
+                    src={options.icon} />
+                    </Link>
+                  ))
+                }
          </div>
 
     </div>
