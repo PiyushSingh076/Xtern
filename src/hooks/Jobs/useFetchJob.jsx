102f6ea519ef7e55a5931d43456612a6a92a4a0d
Roni <64074505+RonishRohan@users.noreply.github.com>
2025-01-03 12:42:40 +0530

    Add useFetchJob Hook

---
+++ src/hooks/Jobs/useFetchJob.jsx
@@ -0,0 +1,41 @@
+import { collection, getDoc, getDocs, query, where } from "firebase/firestore"
+import { db } from "../../firebaseConfig"
+import toast from "react-hot-toast";
+import { useState } from "react";
+export const useFetchJob = ({id}) => {
+    const [jobDetails, setJobDetails] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const handleFetchJob = async () => {
+            setLoading(true);
+            const data = await fetchJob();
+            setJobDetails(data);
+            setLoading(false);
+
+        }
+        handleFetchJob();
+    }, [id])
+
+    async function fetchJob() {
+        const jobSnapshot = await getDoc(doc(db, "jobs", id));
+        if(!jobSnapshot.exists()) {
+            toast.error("Job not found");
+            throw new Error("Job not found");
+        }
+        const data = jobSnapshot.data();
+        data.applicantData = [];
+        return data;
+    }
+
+    async function fetchApplicantDetails() {
+        const job = (await getDoc(doc(db, "jobs", id))).data();
+
+        const applicants = job.applicants;
+
+        return applicants;
+        
+    }
+
+    return { jobDetails, loading, fetchApplicantDetails };
+}
