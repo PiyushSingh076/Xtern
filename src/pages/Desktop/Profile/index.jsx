57192e670d6af4341f68c276fa90540abb1e2976
Amanstudentdiwan <amandubey8833@gmail.com>
2025-01-07 16:04:42 +0530

    Hire Expert Button

--- src/pages/Desktop/Profile/index.jsx
+++ src/pages/Desktop/Profile/index.jsx
@@ -469,7 +469,7 @@ const SingleMentor = () => {
               <div className="service-list">
                 {profileData.serviceDetails.map((item, index) => (
                   <div
-                    // onClick={() => handleService(item)}
+                    onClick={() => handleService(item)}
                     className="service-item"
                     key={index}
                     style={{
